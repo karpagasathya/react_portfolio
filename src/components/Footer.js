7fb7bfd2448f01d32cd7b864b9f88a80e8fac1e4
sathya <karpagasathya@gmail.com>
2020-08-20 00:46:38 -0500

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="uk-text-center uk-text-middle footer fixed-bottom">
+      <div class="">© 2020 Copyright: SG</div>
+    </footer>
+  );
+};
+
+export default Footer;
