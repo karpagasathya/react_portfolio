e7f8278d6c2d1404d14e558b3c0f5795d06ae06d
sathya <karpagasathya@gmail.com>
2020-08-20 00:46:05 -0500

    navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./style.css";
+
+
+const MyNavbar = () => {
+  return (
+    <div>
+      <Navbar fixed="top" variant="dark" expand="md" className="animate-navbar nav-theme justify-content-between">
+        <div>
+          <Navbar.Brand href="#home">
+            <h5>SathyaGuru</h5>
+          </Navbar.Brand>
+        </div>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto ">
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#resume">Resume</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MyNavbar;
