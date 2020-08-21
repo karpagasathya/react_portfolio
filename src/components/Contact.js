60eea013d8e54a8227d48d7f6b9c5a40db572f05
sathya <karpagasathya@gmail.com>
2020-08-21 00:08:17 -0500

    contact page redesigned using uikit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,60 +1,44 @@
 import React from "react";
+import "./style.css";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 
-const ContactForm = () => {
+
+function Contact() {
   return (
-    <div id="contact">
-      <h1 className="pt-4 text-center font-details-b pb-2">CONTACT ME</h1>
-      <Jumbotron className="contact-jumbotron">
-        <Row className="">
-          <Col>
-            <div className="col-md">
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item border-top-0">
-                  <i className="fas fa-phone-square"></i> 6128402890
-                </li>
-                <li className="list-group-item">
-                  <i class="fas fa-envelope"></i> karpagasathya@gmail.com
-                </li>
-                <li className="list-group-item">
-                  <i className="fas fa-home"></i>
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col className="d-flex justify-content-center flex-wrap">
-            <div className="m-2">
-              <a href="mailto:karpagasathya@gmail.com" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-danger">
-                  <i className="fas fa-envelope-square"></i> Email Me
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a href="https://www.linkedin.com/in/karpagasathya-gurupandi-33bb0690/" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-primary" title="Visit my LinkedIn">
-                  <i className="fab fa-linkedin"></i> LinkedIn
-                </Button>
-              </a>
-            </div>
-            <div className="m-2">
-              <a href="https://github.com/karpagasathya" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-light" title="My other projects">
-                  <i className="fab fa-github-square"></i> GitHub
-                </Button>
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Jumbotron>
-    </div>
+    <Jumbotron className="contact-jumbotron">
+      <div>
+        <h3 id="contact-title">CONTACT ME</h3>
+        <ul className="uk-list">
+          <li>
+            <a uk-icon="icon: mail" className="contact-icon contact-link-icon" href="mailto:karpagasathya@gmail.com"></a>
+            <a className="contact-link contact-li uk-text-middle" href="mailto:karpagasathya@gmail.com">
+              karpagasathya@gmail.com
+            </a>
+          </li>
+          <li>
+            <span className="contact-icon" uk-icon="icon: receiver"></span>
+            <span className="contact-li uk-text-middle">6128402890</span>
+          </li>
+          <li>
+            <span uk-icon="icon: home" className="contact-icon"></span>
+            <span className="contact-li uk-text-bottom">Minnetonka, Minneapolis, Minnesota.</span>
+          </li>
+          <li>
+            <a uk-icon="icon: github" href="https://github.com/karpagasathya" className="contact-icon contact-link-icon" target="_blank" rel="noopener noreferrer"></a>
+            <a className="contact-link contact-li uk-text-middle" href="https://github.com/karpagasathya" target="_blank" rel="noopener noreferrer">
+              karpagasathya
+            </a>
+          </li>
+          <li>
+            <a uk-icon="icon: linkedin" href="https://www.linkedin.com/in/karpagasathya-gurupandi-33bb0690/" className="contact-icon contact-link-icon" target="_blank" rel="noopener noreferrer"></a>
+            <a className="contact-link contact-li uk-text-bottom" href="https://www.linkedin.com/in/karpagasathya-gurupandi-33bb0690/" target="_blank" rel="noopener noreferrer">
+              karpagasathya
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Jumbotron>
   );
-};
+}
 
-export default ContactForm;
+export default Contact;
