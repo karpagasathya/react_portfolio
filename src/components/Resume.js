6e345ce776856c612fea097090bbc1df0684898c
sathya <karpagasathya@gmail.com>
2020-08-20 21:54:49 -0500

    resume tab added

---
+++ src/components/Resume.js
@@ -0,0 +1,18 @@
+import React from "react";
+import resume from "../assets/images/Karpagasathya.pdf";
+
+function ResumeModal() {
+  console.log("coming inside resume modal")
+  return (
+    <div id="resume-modal" className="uk-flex-top" uk-modal="true">
+      <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+        <button className="uk-modal-close-default" type="button" uk-close="true"></button>
+        <p>
+          <embed src={resume} type="application/pdf" width="100%" height="600px" />
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ResumeModal;
