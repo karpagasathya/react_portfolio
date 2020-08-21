0e1e47471f5c6247aeea85785d4802aac080cf2a
sathya <karpagasathya@gmail.com>
2020-08-20 21:55:34 -0500

    portfolio page

--- src/pages/portfolioPage.js
+++ src/pages/portfolioPage.js
@@ -3,14 +3,24 @@ import "../components/style.css";
 import PortfolioCard from "../components/Portfolio";
 import portfolioData from "../utils/portfolioData";
 
+const style = {
+  portfolio: {
+        backgroundColor: "blue",
+        margin: "20px",
+        padding:"30px",
+  },
+};
+
 function PortfolioPage() {
   return (
     <>
-      <h1 className="pt-3 text-center font-details pb-3">PORTFOLIO</h1>
-      <div className="uk-child-width-1-2@m uk-grid-match" uk-grid="true" id="portfolio-section" uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 250; repeat: false">
-        {portfolioData.map((portfolio) => (
-          <PortfolioCard title={portfolio.title} about={portfolio.about} repoURL={portfolio.repoURL} siteURL={portfolio.siteURL} imgURL={portfolio.imgURL} />
-        ))}
+      <div style={style.portfolio}>
+        <h1 className="pt-3 text-center font-details pb-3">PORTFOLIO</h1>
+        <div className="uk-child-width-1-2@m uk-grid-match" uk-grid="true" id="portfolio-section" uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 250; repeat: false">
+          {portfolioData.map((portfolio) => (
+            <PortfolioCard title={portfolio.title} about={portfolio.about} repoURL={portfolio.repoURL} siteURL={portfolio.siteURL} imgURL={portfolio.imgURL} />
+          ))}
+        </div>
       </div>
     </>
   );
