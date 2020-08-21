4727f5295246933b309d5b544ca1d1ef3a81837e
sathya <karpagasathya@gmail.com>
2020-08-20 21:55:50 -0500

    skills data

---
+++ src/utils/skillsData.js
@@ -0,0 +1,13 @@
+const clientSide = [
+  "React",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "jQuery",
+  "Bootstrap, UIKit",
+];
+
+const serverSide = ["Node.js", "Express.js", "Handlebars", "MySQL ", "Sequelize", "MongoDB"];
+
+
+export { clientSide, serverSide };
