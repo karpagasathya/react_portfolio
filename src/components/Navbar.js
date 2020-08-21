6c94e26ab68f5b7e5c2599cb5341b54067887ec5
sathya <karpagasathya@gmail.com>
2020-08-20 21:58:11 -0500

    for resume link the model added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,9 @@ const MyNavbar = () => {
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#portfolio">Portfolio</Nav.Link>
               <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#resume">Resume</Nav.Link>
+              <Nav.Link uk-toggle="target: #resume-modal" href="#resume-modal">
+                Resume
+              </Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
