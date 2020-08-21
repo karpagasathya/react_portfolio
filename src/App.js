706637f51a43d06952711b48525106fb6f3e3796
sathya <karpagasathya@gmail.com>
2020-08-20 21:57:25 -0500

    main app file with all components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { HashRouter as Router, Route } from "react-router-dom";
+import About from './components/About';
+// import Slide from "react-reveal/Slide";
+// import Fade from "react-reveal/Fade";
+// import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
+import ContactForm from './components/Contact';
+import PortfolioPage from './pages/portfolioPage';
+import SkillsPage from "./pages/skillsPage";
+import ResumeModal from "./components/Resume"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="App" style={{ position: "relative" }}>
+      <Router>
+        <div>
+          <Navbar />
+          <div>
+            <Container className="container-box rounded">
+              <Route exact path="/" component={About} />
+            </Container>
+          </div>
+          <div>
+            <Container className="container-box rounded">
+              <Route exact path="/about" component={About} />
+            </Container>
+          </div>
+          <div>
+            <Container className="container-box rounded">
+              <Route exact path="/portfolio" component={PortfolioPage} />
+            </Container>
+          </div>
+          <div>
+            <Container className="container-box rounded">
+              <Route exact path="/skills" component={SkillsPage} />
+            </Container>
+          </div>
+          <div>
+            <Container className="container-box rounded">
+              <Route exact path="/contact" component={ContactForm} />
+            </Container>
+          </div>
+          {/* <div>
+            <Container className="container-box rounded">
+              <Route exact path="/modal-example" component={ResumeModal} />
+            </Container>
+          </div> */}
+        </div>
+        <ResumeModal />
+        <Footer />
+      </Router>
     </div>
   );
 }
