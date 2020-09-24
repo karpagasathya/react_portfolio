b403491ba7ec548d10cd02783743ec72eb19d725
sathya <karpagasathya@gmail.com>
2020-09-24 02:16:27 -0500

    Added final project to portfolio

--- src/utils/portfolioData.js
+++ src/utils/portfolioData.js
@@ -1,11 +1,12 @@
 import burgerPic from "../assets/images/portfolio/burger.jpg";
 import booksPic from "../assets/images/portfolio/books.jpg";
-import workoutPic from "../assets/images/portfolio/fitness.jpg";
+// import workoutPic from "../assets/images/portfolio/fitness.jpg";
 import employeeDirPic from "../assets/images/portfolio/employee.png";
 import todoPic from "../assets/images/portfolio/todo-list.jpg";
 import pwaPic from "../assets/images/portfolio/pwa.png";
 import weatherPic from "../assets/images/portfolio/weather.jpg";
 import notePic from "../assets/images/portfolio/note.png";
+import forumPic from "../assets/images/portfolio/forum.jpg";
 
 function ProjectCard(title, about, imgURL, siteURL, repoURL) {
   this.title = title;
@@ -15,17 +16,17 @@ function ProjectCard(title, about, imgURL, siteURL, repoURL) {
   this.repoURL = repoURL;
 }
 
-const workoutCard = new ProjectCard(
-  "Workout Tracker",
-  "An application for tracking your daily workouts and fitness progress. Built using Express, MongoDB, and Mongoose.",
-  workoutPic,
-  "https://frozen-scrubland-57003.herokuapp.com/",
-  "https://github.com/karpagasathya/workout_tracker"
+const forumCard = new ProjectCard(
+  "Spark Forums",
+  "A MERN Stack application that allows users to create a discussion or start a conversation by posting some messages. Built using React, Node.js, Express.js, Mongoose, Material UI, HTML.",
+  forumPic,
+  "https://spark-forums.herokuapp.com/",
+  "https://github.com/schoess/Open_Forums"
 );
 
 const employeeDirCard = new ProjectCard(
   "Employee Directory",
-  "An application for accessing employee information - built with React.",
+  "An application for accessing employee information - built with React, Node.js, HTML, Bootstrap.",
   employeeDirPic,
   "https://immense-dusk-32798.herokuapp.com/",
   "https://github.com/karpagasathya/react_employee_directory"
@@ -33,7 +34,7 @@ const employeeDirCard = new ProjectCard(
 
 const burgerCard = new ProjectCard(
   "Eat-Da-Burger",
-  "This Application is a burger logger in which user can create their own new burgers to devour. Built with Node.js,Express,Handlebars,MySql, ORM(Object Relational Mapping) and MVC pattern.",
+  "This Application is a burger logger in which user can create their own new burgers to devour. Built with Node.js,Express.js, Bootstrap, Handlebars, MySql, ORM(Object Relational Mapping) and MVC pattern.",
   burgerPic,
   "https://rocky-meadow-23943.herokuapp.com",
   "https://github.com/karpagasathya/MVC_Eat_Da_Burger"
@@ -41,7 +42,7 @@ const burgerCard = new ProjectCard(
 
 const booksCard = new ProjectCard(
   "Buy the Book",
-  "This Application is an online Bookstore to buy some books from six different genres. Built with Node.js,Express server,Handlebars,Sequelize and MVC pattern.",
+  "This Application is an online Bookstore to buy some books from six different genres. Built with Node.js,Express.js , Handlebars, Sequelize, Materialize and MVC pattern.",
   booksPic,
   "https://buy-the-book.herokuapp.com",
   "https://github.com/karpagasathya/buy_the_book"
@@ -79,10 +80,6 @@ const noteCard = new ProjectCard(
   "https://github.com/karpagasathya/note_taker"
 );
 
-
-
-
-
-let portfolioData = [workoutCard, employeeDirCard, burgerCard, booksCard, todoCard, weatherCard,budgetCard,noteCard];
+let portfolioData = [forumCard, employeeDirCard, burgerCard, booksCard, todoCard, weatherCard, budgetCard, noteCard];
 
 export default portfolioData;
--- src/utils/skillsData.js
+++ src/utils/skillsData.js
@@ -1,14 +1,7 @@
-const skill1 = [
-  "React",
-  "JavaScript",
-  "jQuery",
-  "Redux",
-  "React Router"
-];
+const skill1 = ["React", "JavaScript", "jQuery", "Redux", "React Router", "Lodash"];
 
-const skill2 = ["Node.js", "Express.js", "Handlebars", "MySQL ", "Sequelize", "MongoDB"];
+const skill2 = ["Node.js", "Express.js", "Handlebars", "MySQL ", "Sequelize", "MongoDB", "Mongoose"];
 
-const skill3 = ["HTML", "CSS", "Bootstrap", "UIKit","React-Bootstrap"];
-
+const skill3 = ["HTML", "CSS", "Bootstrap", "UIKit", "React-Bootstrap", "Material UI"];
 
 export { skill1, skill2, skill3 };
